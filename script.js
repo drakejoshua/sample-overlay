6580c89f791471d997a2aa7b76ac8cb3720950dc
Joshua Mabawonku <drakejoshua086@gmail.com>
2023-07-26 13:53:08 +0100

    ADDED INITIAL FILES
    - added the initial files for the repo( index.html, style.css )

---
+++ script.js
@@ -0,0 +1,9 @@
+function opensidenav() {
+    document.getElementsByTagName('aside')[0].style.width = "20%";
+    document.getElementsByTagName('aside')[0].style.padding = "10px";
+}
+
+function closesidenav() {
+    document.getElementsByTagName('aside')[0].style.width = "0%";
+    document.getElementsByTagName('aside')[0].style.padding = "0px";
+}
